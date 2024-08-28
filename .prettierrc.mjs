0405bcacee592aa1d83c68454796b1cbd44c361b
olzhas-kalikhan <olzhas-kalikhan@outlook.com>
2024-08-28 18:52:13 -0400

    project setup + draft layout

---
+++ .prettierrc.mjs
@@ -0,0 +1,17 @@
+/** @type {import("prettier").Config} */
+
+export default {
+  plugins: [
+    "prettier-plugin-astro",
+    "prettier-plugin-tailwindcss",
+    "@prettier/plugin-xml",
+  ],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
